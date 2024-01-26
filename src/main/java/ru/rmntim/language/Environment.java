6beda9ba8c47a2628a6e0d2b661a266b6ea04b15
Roman Timoshkin <rmntim@yandex.ru>
2024-01-26 09:30:34 +0300

    refactor: change Environment so it accepts LoxCallable

--- src/main/java/ru/rmntim/language/Environment.java
+++ src/main/java/ru/rmntim/language/Environment.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
-    private final Map<String, Variable> variables = new HashMap<>();
+    private final Map<String, Object> variables = new HashMap<>();
     private final Environment parent;
 
     public Environment() {
@@ -17,18 +17,22 @@ public class Environment {
         this.parent = parent;
     }
 
-    public void define(String name, Variable value) {
+    public void define(String name, Object value) {
         variables.put(name, value);
     }
 
     public Object get(Token name) {
         if (variables.containsKey(name.literal())) {
-            var variable = variables.get(name.literal());
-            if (variable.isInitialized()) {
-                return variable.getValue();
-            }
+            var value = variables.get(name.literal());
+            if (value instanceof Variable variable) {
+                if (variable.isInitialized()) {
+                    return variable.getValue();
+                }
 
-            throw new RuntimeError(name, "Uninitialized variable '" + name.literal() + "'");
+                throw new RuntimeError(name, "Uninitialized variable '" + name.literal() + "'");
+            } else if (value instanceof LoxCallable callable) {
+                return callable;
+            }
         }
 
         if (parent != null) {
@@ -41,9 +45,13 @@ public class Environment {
     public void assign(Token name, Object newValue) {
         if (variables.containsKey(name.literal())) {
             var value = variables.get(name.literal());
-            value.setValue(newValue);
-            value.setInitialized();
-            return;
+            if (value instanceof Variable variable) {
+                variable.setValue(newValue);
+                variable.setInitialized();
+                return;
+            } else if (value instanceof LoxCallable) {
+                variables.put(name.literal(), newValue);
+            }
         }
 
         if (parent != null) {
